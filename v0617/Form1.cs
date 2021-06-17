4e2a9b1231dce3ca0e5554764b8fec332334acc0
A601-18 <tnk.dat.class@gmail.com>
2021-06-17 09:55:34 +0900

    復習問題

--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -12,9 +12,28 @@ namespace v0617
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = 10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int.Parse(label1.Left);
+            int.Parse()
+            
+            label1.Left = vx;
+            label1.Top = vy;
+            if (vx <= -1)
+            {
+                vx = -1;
+            }
+            if (vy <= -1)
+            {
+                vy = -1;
+            }
         }
 
     }
